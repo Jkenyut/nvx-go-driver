8a57ad6561623c268b61416d12d4b16b4b470ae1
satria nur saputro <satrianursaputro06@gmail.com>
2026-05-10 01:07:00 +0700

    rabbit mq enchance

---
+++ rabbitmq/consumer.go
@@ -0,0 +1,267 @@
+package rabbitmq
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Action int
+
+const (
+	ActionAck Action = iota
+	ActionNackRequeue
+	ActionNackDiscard
+)
+
+type HandlerFunc func(
+	ctx context.Context,
+	msg amqp.Delivery,
+) Action
+
+type Consumer struct {
+	client *Client
+
+	queue string
+
+	autoAck bool
+	qos     int
+
+	lock sync.RWMutex
+
+	ch *amqp.Channel
+
+	done chan struct{}
+	wg   sync.WaitGroup
+}
+
+func NewConsumer(
+	client *Client,
+	queue string,
+) *Consumer {
+
+	return &Consumer{
+		client:  client,
+		queue:   queue,
+		autoAck: false,
+		qos:     1,
+		done:    make(chan struct{}),
+	}
+}
+
+func (c *Consumer) SetQos(
+	qos int,
+) {
+	c.qos = qos
+}
+
+func (c *Consumer) SetAutoAck(
+	autoAck bool,
+) {
+	c.autoAck = autoAck
+}
+
+func (c *Consumer) Start(
+	ctx context.Context,
+	handler HandlerFunc,
+) {
+
+	c.wg.Add(1)
+
+	go func() {
+		defer c.wg.Done()
+
+		c.loop(ctx, handler)
+	}()
+}
+
+func (c *Consumer) loop(
+	ctx context.Context,
+	handler HandlerFunc,
+) {
+
+	for {
+
+		select {
+		case <-ctx.Done():
+			return
+
+		case <-c.done:
+			return
+
+		default:
+		}
+
+		err := c.consume(ctx, handler)
+		if err != nil {
+
+			c.client.log.Error().
+				Err(err).
+				Str("queue", c.queue).
+				Msg("Consumer stopped")
+		}
+
+		select {
+
+		case <-ctx.Done():
+			return
+
+		case <-c.done:
+			return
+
+		case <-time.After(5 * time.Second):
+		}
+	}
+}
+
+func (c *Consumer) consume(
+	ctx context.Context,
+	handler HandlerFunc,
+) error {
+
+	ch, err := c.client.NewChannel()
+	if err != nil {
+		return err
+	}
+
+	c.lock.Lock()
+	c.ch = ch
+	c.lock.Unlock()
+
+	defer func() {
+
+		c.lock.Lock()
+		c.ch = nil
+		c.lock.Unlock()
+
+		if !ch.IsClosed() {
+			_ = ch.Close()
+		}
+	}()
+
+	err = ch.Qos(
+		c.qos,
+		0,
+		false,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	consumerTag := fmt.Sprintf(
+		"%s-%d",
+		c.queue,
+		time.Now().UnixNano(),
+	)
+
+	msgs, err := ch.Consume(
+		c.queue,
+		consumerTag,
+		c.autoAck,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	closeChan := ch.NotifyClose(
+		make(chan *amqp.Error, 1),
+	)
+
+	for {
+
+		select {
+
+		case <-ctx.Done():
+			return nil
+
+		case <-c.done:
+			return nil
+
+		case msg, ok := <-msgs:
+
+			if !ok {
+				return errors.New(
+					"delivery channel closed",
+				)
+			}
+
+			func() {
+
+				defer func() {
+
+					if recover() != nil {
+
+						c.client.log.Error().
+							Str("queue", c.queue).
+							Msg("Consumer panic recovered")
+
+						if !c.autoAck {
+							_ = msg.Nack(false, true)
+						}
+					}
+				}()
+
+				action := handler(ctx, msg)
+
+				if !c.autoAck {
+					switch action {
+					case ActionAck:
+						_ = msg.Ack(false)
+					case ActionNackRequeue:
+						_ = msg.Nack(false, true)
+					case ActionNackDiscard:
+						_ = msg.Nack(false, false)
+					default:
+						// Safe fallback: Discard on unknown action
+						c.client.log.Warn().
+							Int("action", int(action)).
+							Str("queue", c.queue).
+							Msg("Unknown consumer action, discarding message")
+						_ = msg.Nack(false, false)
+					}
+				}
+			}()
+
+		case err := <-closeChan:
+
+			if err != nil {
+				return err
+			}
+
+			return errors.New(
+				"consumer channel closed",
+			)
+		}
+	}
+}
+
+func (c *Consumer) Close() {
+
+	select {
+	case <-c.done:
+	default:
+		close(c.done)
+	}
+
+	c.lock.RLock()
+
+	ch := c.ch
+
+	c.lock.RUnlock()
+
+	if ch != nil && !ch.IsClosed() {
+		_ = ch.Close()
+	}
+
+	c.wg.Wait()
+}
---
+++ rabbitmq/publisher.go
@@ -0,0 +1,326 @@
+package rabbitmq
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Publisher struct {
+	client *Client
+
+	lock sync.RWMutex
+
+	ch *amqp.Channel
+
+	publishLock     sync.Mutex
+	pendingConfirms sync.Map
+
+	done chan struct{}
+	wg   sync.WaitGroup
+}
+
+func NewPublisher(
+	client *Client,
+) (*Publisher, error) {
+
+	ch, err := client.NewChannel()
+	if err != nil {
+		return nil, err
+	}
+
+	err = ch.Confirm(false)
+	if err != nil {
+		_ = ch.Close()
+		return nil, err
+	}
+
+	p := &Publisher{
+		client: client,
+		ch:     ch,
+		done:   make(chan struct{}),
+	}
+
+	p.startConfirmListener(ch)
+
+	p.wg.Add(1)
+
+	go func() {
+		defer p.wg.Done()
+		p.reconnectLoop()
+	}()
+
+	return p, nil
+}
+
+func (p *Publisher) startConfirmListener(ch *amqp.Channel) {
+	confirmChan := ch.NotifyPublish(make(chan amqp.Confirmation, 1000))
+
+	p.wg.Add(1)
+	go func() {
+		defer p.wg.Done()
+		for conf := range confirmChan {
+			if chObj, ok := p.pendingConfirms.LoadAndDelete(conf.DeliveryTag); ok {
+				resultChan := chObj.(chan error)
+				if conf.Ack {
+					resultChan <- nil
+				} else {
+					resultChan <- errors.New("message not acknowledged by broker")
+				}
+			}
+		}
+	}()
+}
+
+func (p *Publisher) reconnectLoop() {
+
+	backoff := time.Second
+
+	for {
+
+		select {
+		case <-p.done:
+			return
+		default:
+		}
+
+		p.lock.RLock()
+		ch := p.ch
+		p.lock.RUnlock()
+
+		if ch == nil {
+
+			select {
+			case <-p.done:
+				return
+			case <-time.After(backoff):
+			}
+
+			continue
+		}
+
+		closeChan := ch.NotifyClose(
+			make(chan *amqp.Error, 1),
+		)
+
+		err, ok := <-closeChan
+
+		select {
+		case <-p.done:
+			return
+		default:
+		}
+
+		// channel already replaced
+		p.lock.RLock()
+		currentCh := p.ch
+		p.lock.RUnlock()
+
+		if currentCh != ch {
+			continue
+		}
+
+		// graceful close
+		if !ok || err == nil {
+
+			select {
+			case <-p.done:
+				return
+			case <-time.After(backoff):
+			}
+
+			continue
+		}
+
+		p.client.log.Warn().
+			Err(err).
+			Msg("RabbitMQ publisher channel lost")
+
+		for {
+
+			select {
+			case <-p.done:
+				return
+			default:
+			}
+
+			newCh, e := p.client.NewChannel()
+			if e != nil {
+
+				p.client.log.Error().
+					Err(e).
+					Msg("Failed to recreate publisher channel")
+
+				select {
+				case <-p.done:
+					return
+				case <-time.After(backoff):
+				}
+
+				if backoff < 30*time.Second {
+					backoff *= 2
+				}
+
+				continue
+			}
+
+			e = newCh.Confirm(false)
+			if e != nil {
+
+				_ = newCh.Close()
+
+				p.client.log.Error().
+					Err(e).
+					Msg("Failed to enable publisher confirm mode")
+
+				select {
+				case <-p.done:
+					return
+				case <-time.After(backoff):
+				}
+
+				if backoff < 30*time.Second {
+					backoff *= 2
+				}
+
+				continue
+			}
+
+			p.lock.Lock()
+
+			oldCh := p.ch
+			p.ch = newCh
+
+			p.lock.Unlock()
+
+			p.startConfirmListener(newCh)
+
+			if oldCh != nil && !oldCh.IsClosed() {
+				_ = oldCh.Close()
+			}
+
+			backoff = time.Second
+
+			p.client.log.Info().
+				Msg("RabbitMQ publisher channel recreated")
+
+			break
+		}
+	}
+}
+
+func (p *Publisher) channel() (*amqp.Channel, error) {
+
+	p.lock.RLock()
+
+	ch := p.ch
+
+	p.lock.RUnlock()
+
+	if ch == nil || ch.IsClosed() {
+		return nil, errors.New(
+			"publisher channel unavailable",
+		)
+	}
+
+	return ch, nil
+}
+
+func (p *Publisher) Publish(
+	ctx context.Context,
+	exchange string,
+	routingKey string,
+	msg amqp.Publishing,
+	mandatory bool,
+	immediate bool,
+) error {
+
+	var lastErr error
+
+	for i := 0; i < 3; i++ {
+
+		p.publishLock.Lock()
+
+		ch, err := p.channel()
+		if err != nil {
+			p.publishLock.Unlock()
+			lastErr = err
+			time.Sleep(time.Second)
+			continue
+		}
+
+		seqNo := ch.GetNextPublishSeqNo()
+		resultChan := make(chan error, 1)
+		p.pendingConfirms.Store(seqNo, resultChan)
+
+		err = ch.PublishWithContext(
+			ctx,
+			exchange,
+			routingKey,
+			mandatory,
+			immediate,
+			msg,
+		)
+
+		if err != nil {
+			p.pendingConfirms.Delete(seqNo)
+			p.publishLock.Unlock()
+			lastErr = err
+			time.Sleep(time.Second)
+			continue
+		}
+
+		p.publishLock.Unlock()
+
+		select {
+
+		case resErr := <-resultChan:
+
+			if resErr != nil {
+				lastErr = resErr
+				time.Sleep(time.Second)
+				continue
+			}
+
+			return nil
+
+		case <-ctx.Done():
+			p.pendingConfirms.Delete(seqNo)
+			return ctx.Err()
+
+		case <-time.After(5 * time.Second):
+			p.pendingConfirms.Delete(seqNo)
+			lastErr = errors.New("publisher confirm timeout")
+			time.Sleep(time.Second)
+		}
+	}
+
+	return lastErr
+}
+
+func (p *Publisher) Close() error {
+
+	select {
+	case <-p.done:
+	default:
+		close(p.done)
+	}
+
+	p.lock.Lock()
+
+	ch := p.ch
+	p.ch = nil
+
+	p.lock.Unlock()
+
+	if ch != nil && !ch.IsClosed() {
+		_ = ch.Close()
+	}
+
+	p.wg.Wait()
+
+	return nil
+}
